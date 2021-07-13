7c30f7c0c8cc501f8fb3633b08fc9f0cac629abb
vineet pant <10172895+vineetpant@users.noreply.github.com>
2021-07-13 20:34:12 +0200

    dynamic url and did

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use serde::{Deserialize, Serialize};
+use std::env;
 
 #[derive(Serialize, Deserialize)]
 struct DIDResolveResponse {}
 
+
+
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://dev.uniresolver.io/1.0/identifiers/did:ethr:mainnet:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736")?
+    // ToDo: replace did , replace host with config value
+    let args: Vec<String> = env::args().collect();
+    let did = &args[1];
+    let mut resolver_url = env::var("RESOLVER_URL").ok().unwrap();
+    resolver_url.push_str(did);
+
+
+    let resp = reqwest::blocking::get(&resolver_url)?
         .text()?;
     println!("{:#?}", resp);
+    println!("{:?}", resolver_url);
+
     Ok(())
 }
