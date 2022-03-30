6115fd75f7ea3f5cfbd47e19afbc084eece17eaa
vineet pant <10172895+vineetpant@users.noreply.github.com>
2022-03-30 09:02:20 +0200

    change return res

--- src/vade_universal_resolver.rs
+++ src/vade_universal_resolver.rs
@@ -138,12 +138,13 @@ impl VadePlugin for VadeUniversalResolver {
                     &mut res as *mut *mut c_char);
 
                 println!("error_code {}",error_code);
-                let res = unsafe { CStr::from_ptr(res).to_string_lossy().into_owned() };
-                println!("res {}",res);
+
                 if error_code < 0 {
                     return Err(Box::from(format!("{}", error_code)));
                 }
-
+                
+                let res = unsafe { CStr::from_ptr(res).to_string_lossy().into_owned() };
+                println!("res {}",res);   
                 return Ok(VadePluginResultValue::Success(Some(res.to_string())));
               } else {
 
